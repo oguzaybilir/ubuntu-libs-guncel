89a3858f5f95d73f8e73b803c47c571bbef8e3c5
comp5 <b210104038@subu.edu.tr>
2024-06-07 14:15:20 +0300

    .gitignore and tensorrt updated

--- 8-tensorrt-installation.sh
+++ 8-tensorrt-installation.sh
@@ -1,8 +1,7 @@
-gdown --id 18atV9LgLGsjU7-ztCsneU8nUhshw3gr8
-sudo dpkg -i nv-tensorrt-local-repo-ubuntu2204-10.0.1-cuda-12.4_1.0-1_amd64.deb
-sudo cp /var/nv-tensorrt-local-repo-*/*-keyring.gpg /usr/share/keyrings/
-sudo apt-get update
-sudo apt-get install tensorrt
-sudo apt-get install python3-libnvinfer-dev
+python3 -m pip install --upgrade pip
+python3 -m pip install wheel
+python3 -m pip install --upgrade tensorrt
+python3 -m pip install --upgrade tensorrt_lean
+python3 -m pip install --upgrade tensorrt_dispatch
 
 
