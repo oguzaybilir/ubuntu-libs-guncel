a9beeb65b8f74c7bed6fbb73aedb05c544e222c5
comp5 <b210104038@subu.edu.tr>
2024-06-07 14:53:52 +0300

    pip3 alias added

--- 2-pip-installation.sh
+++ 2-pip-installation.sh
@@ -1,3 +1,4 @@
 sudo apt install python3-pip
 pip3 install --upgrade pip setuptools
 echo "export PATH=\"/home/$USER/.local/bin:\$PATH\"" >> ~/.bashrc && source ~/.bashrc
+echo "alias pip3=\"python3 -m pip\"" >> ~/.bashrc && source ~/.bashrc
