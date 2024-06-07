317994083e7bacdf8d9a800992844ed072535ff2
comp5 <b210104038@subu.edu.tr>
2024-06-07 14:20:07 +0300

    cuda updated

--- 5-cuda-installation.sh
+++ 5-cuda-installation.sh
@@ -5,3 +5,9 @@ sudo dpkg -i cuda-repo-ubuntu2004-12-2-local_12.2.0-535.54.03-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2004-12-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get -y install cuda
+
+echo 'export PATH=/usr/local/cuda-12.2/bin${PATH:+:${PATH}}' >> ~/.bashrc 
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.2/lib64' >> ~/.bashrc 
+source ~/.bashrc
+
+sudo reboot
--- 6-export-cuda-path.sh
+++
@@ -1,5 +0,0 @@
-echo 'export PATH=/usr/local/cuda-12.2/bin${PATH:+:${PATH}}' >> ~/.bashrc 
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.2/lib64' >> ~/.bashrc 
-source ~/.bashrc
-
-nvcc -V
