2a859e75852e803ebc345b01da5a2e5dc1820a05
comp5 <b210104038@subu.edu.tr>
2024-08-09 01:47:42 +0300

    ll

--- 8-tensorrt-installation.sh
+++ 8-tensorrt-installation.sh
@@ -1,6 +1,7 @@
-gdown --id 1LNqYO3uERDgjp6Ty_p1QOQff2AY8iZyR
-sudo dpkg -i nv-tensorrt-local-repo-ubuntu2004-8.6.1-cuda-11.8_1.0-1_amd64.deb
-sudo cp /var/nv-tensorrt-local-repo-/-keyring.gpg /usr/share/keyrings/
+sudo apt install wget
+wget https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.0.1/local_repo/nv-tensorrt-local-repo-ubuntu2004-10.0.1-cuda-12.4_1.0-1_amd64.deb
+sudo dpkg -i nv-tensorrt-local-repo-ubuntu2004-10.0.1-cuda-12.4_1.0-1_amd64.deb
+sudo cp /var/nv-tensorrt-local-repo-ubuntu2004-10.0.1-cuda-12.4/nv-tensorrt-local-4BE0C9B6-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get install tensorrt
 sudo apt-get install python3-libnvinfer-dev
