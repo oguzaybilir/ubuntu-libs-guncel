e17c802508178524a6bef91d161de2198fcd3e78
comp1 <oguzaybilir@gmail.com>
2025-01-25 19:22:28 +0300

    ll

--- 5-cuda-installation.sh
+++ 5-cuda-installation.sh
@@ -9,4 +9,3 @@ sudo apt-get -y install cuda
 echo 'export PATH=/usr/local/cuda-12.2/bin${PATH:+:${PATH}}' >> ~/.bashrc 
 echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.2/lib64' >> ~/.bashrc 
 source ~/.bashrc
-sudo reboot
